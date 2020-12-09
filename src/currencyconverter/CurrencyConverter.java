f48388fd083260d3816ad1f1ef65b57fd38ff371
Luiz Ângelo Martins <Luiz ngelo Martins@LuizAngelo>
2020-12-09 21:14:36 -0200

    Commit feito para registrar o início do projeto:
    	- Criação do projeto.
    	- Implementação do projeto no repositório git.

---
+++ src/currencyconverter/CurrencyConverter.java
@@ -0,0 +1,27 @@
+
+
+
+    // [ "CRIADOR: LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE PRINCIPAL:
+package currencyconverter;
+
+
+public class CurrencyConverter {
+
+    
+    // MÉTODO PRINCIPAL - MAIN:
+    public static void main(String[] args) {
+    
+        // MAIN
+        
+    }
+    
+    
+}
+
+
+
+
