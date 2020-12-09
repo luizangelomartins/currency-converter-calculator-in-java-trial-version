a00b83c9529e9f367367adeeec38445c7e8056fa
Luiz Ângelo Martins <Luiz ngelo Martins@LuizAngelo>
2020-12-09 21:27:34 -0200

    Commit feito para registrar alterações no projeto:
    	- Criação da classe que irá gerar e exibir o menu geral.
    	- Criação da classe que irá gerar e exibir a lista de conversão.

---
+++ src/currencyconverter/Menu.java
@@ -0,0 +1,26 @@
+
+
+
+    // [ "CRIADOR: LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE:
+package currencyconverter;
+
+
+// MÉTODO PRINCIPAL DA CLASSE:
+public class Menu {
+    
+    // FUNÇÃO QUE IRÁ EXIBIR O MENU:
+    public void exibir_menu() {
+        
+        // MENU
+        
+    }
+    
+}
+
+
+
+
---
+++ src/funcoes/Lista.java
@@ -0,0 +1,26 @@
+
+
+
+    // [ "CRIADOR: LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE:
+package funcoes;
+
+
+// MÉTODO PRINCIPAL DA CLASSE:
+public class Lista {
+    
+    // FUNÇÃO QUE IRÁ EXIBIR A LISTA DE CONVERSÃO:
+    public void exibir_lista() {
+        
+        // LISTA DE CONVERSÃO
+        
+    }
+    
+}
+
+
+
+
